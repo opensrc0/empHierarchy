d74aea15c3d40e54905d171a9eadf53de1e3dab4
Mr. Gupta <hgupta@quikr.com>
2017-04-15 22:14:11 +0530

    changes emp Hierarchy
    Signed-off-by: Mr. Gupta <hgupta@quikr.com>

---
+++ controller.js
@@ -0,0 +1,17 @@
+app.controller("empCtrl", ['empService' , function(empService) {
+	
+	this.hierarchy = {};
+	this.empJson = empService.getEmpList;
+
+	this.gethierarchy = function (id, pointingId) {
+		const parentPointingId = id;
+		this.hierarchy[parentPointingId] = [];
+		while(pointingId != 0) {
+			if(this.empJson[pointingId].status == 1) {
+				this.hierarchy[parentPointingId].push(this.empJson[pointingId]);
+			}
+			pointingId = this.empJson[pointingId].pointing; 
+		}
+	}
+
+}]);
---
+++ services.js
@@ -0,0 +1,78 @@
+app.service("empService", function() {
+	const obj = {};
+
+	obj.getEmpList = {
+		801 : {
+			"name": "Himanshu Gupta",
+			"empId": 801,
+			"pointing": 501,
+			"image" : '883.jpg',
+			"status" : 1
+		}, 
+		501 : {
+			"name": "Samar Panda",
+			"empId": 501,
+			"pointing": 100,
+			"image" : '766.jpg',
+			"status" : 1
+		},
+		100 : {
+			"name": "Himanshu Sharma",
+			"empId": 100,
+			"pointing": 89,
+			"image" : '553.jpg',
+			"status" : 1
+		},
+		325 : {
+			"name": "Manish Sinha",
+			"empId": 325,
+			"pointing": 9,
+			"image" : '325.jpg',
+			"status" : 1
+		}, 
+		89: {
+			"name": "Manoj Sharma",
+			"empId": 89,
+			"pointing": 9,
+			"image" : '189.jpg',
+			"status" : 1
+		},
+		9 : {
+			"name": "Pranay Chulate",
+			"empId": 9,
+			"pointing" : 0,
+			"image" : '9.jpg',
+			"status" : 1
+		},
+		802: {
+			"name": "Jay Prakash",
+			"empId": 802,
+			"pointing": 501,
+			"image" : '883.jpg',
+			"status" : 1
+		}, 
+		803: {
+			"name": "Gugan N",
+			"empId": 803,
+			"pointing": 100,
+			"image" : '883.jpg',
+			"status" : 1
+		},
+		1000: {
+			"name" : "Nitin Bist",
+			"empId" : 1000,
+			"pointing" : 801,
+			"image" : '1000.jpg',
+			"status" : 1
+		},
+		1001: {
+			"name" : "Mukul",
+			"empId" : 1001,
+			"pointing" : 801,
+			"image" : '1001.jpg',
+			"status" : 1
+		}
+	}
+
+	return obj;
+});
